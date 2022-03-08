c5432f5c1ec5ae1ef3637e2f9e65dfde9a14487a
Sujay HR <93090191+Sujayhr@users.noreply.github.com>
2022-03-08 04:07:53 +0000

    errors

--- p1final.c
+++ p1final.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+void input(int *a,int *b,int *c,int *d)
+{
+  printf("enter two fractions:");
+  scanf("%d%d%d%d",a,b,c,d);
+}
+void add(int a,int b,int c,int d,int *x,int *y)
+{
+*x=(a*d)+(b*c);
+*y=b*d;
+}
+
+void output(int a,int b,int c,int d,int x,int y)
+{
+  printf("the sum of two fractions is %d/%d",x,y);
+}
+int main()
+{
+  int a,b,c,d,x,y;
+  input (&a,&b,&c,&d);
+  add (a,b,c,d,&x,&y);
+  output (a,b,c,d,x,y);
+  return 0;
+}
